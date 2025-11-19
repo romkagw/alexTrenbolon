7f875800c85be75387ffc4bc3aac87dc6a0e2787
roma lushpai <roman.lushpai@onix-systems.com>
2025-11-19 23:34:46 +0200

    first2

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.TELEGRAM_TOKEN; // Токен будем хранить в переменной окружения
+const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
