90d80f2cc942a7ce0c582aa698cc12727bf061c7
roma lushpai <roman.lushpai@onix-systems.com>
2025-11-19 23:24:37 +0200

    first

--- bot.js
+++ bot.js
@@ -1,24 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Вставь сюда токен от BotFather
-const token = '7987249458:AAHbbfaqBZRDenlQuN9e7BKVMJVPqfv46AI';
+const token = process.env.TELEGRAM_TOKEN; // Токен будем хранить в переменной окружения
 
-// Создаем бота с включенным polling (он постоянно проверяет новые сообщения)
 const bot = new TelegramBot(token, { polling: true });
 
-// Команда /start
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Привет! Я простой бот 😎');
+    bot.sendMessage(msg.chat.id, 'Привет! Я бот, размещённый на Railway 😎');
 });
 
-// Команда /help
 bot.onText(/\/help/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Напиши /start чтобы начать');
 });
 
-// Любое сообщение (ответ "Эхо")
 bot.on('message', (msg) => {
-    // Игнорируем команды, чтобы не дублировать
     if (!msg.text.startsWith('/')) {
         bot.sendMessage(msg.chat.id, `Ты написал: ${msg.text}`);
     }
