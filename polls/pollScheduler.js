57e22caeac6bfe83d849532dffd4453e4868dffc
roma lushpai <roman.lushpai@onix-systems.com>
2025-11-21 14:57:41 +0200

    first16

--- polls/pollScheduler.js
+++ polls/pollScheduler.js
@@ -1,11 +1,14 @@
 module.exports = (bot, CHAT_ID) => {
-    const pollCron = '0 15 * * 1,3,5';
-    require('node-cron').schedule(pollCron, () => {
+    const cron = require('node-cron');
+
+    cron.schedule('0 15 * * 1,3,5', () => {
         bot.sendPoll(
             CHAT_ID,
             "Идёте ли вы сегодня в зал? 💪",
             ["Да", "Нет", "Может быть"],
             { is_anonymous: false }
         );
+    }, {
+        timezone: "Europe/Kiev"
     });
 };
